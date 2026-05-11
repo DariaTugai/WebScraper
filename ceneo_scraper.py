06e33ef7c1e0ee05538f31d88b7b19496b1d2f37
DariaTugai <243773@student.uek.krakow.pl>
2026-05-11 17:09:26 +0200

    2026-05-11 DT

--- ceneo_scraper.py
+++ ceneo_scraper.py
@@ -1,7 +1,9 @@
+import os
 import requests
-from bs4 import BeautifulSoup
 import json
-import os 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 product_id = 124893467
 next = True
 headers = {
